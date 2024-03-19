4f4edd7a63eb5d0f1f6faddc5f3e4471891215ce
adlane <adlaneould@gmail.com>
2024-03-19 16:43:27 +0100

    map start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
+    .insert_resource(ClearColor(Color::rgb(0.95, 0.62, 0.07)))
+    .add_systems(Startup, add_people)
+    .run();
+ }
+ 
+ fn hello_world() {
+    println!("hello world");
+ }
+
+ #[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Elaina Proctor" {
+            name.0 = "Elaina Hume".to_string();
+            break; // We don’t need to change any other names
+        }
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
